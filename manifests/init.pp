4f1c43d641f4e6f867c8fa9f770c3c782548021c
Joeri Verdeyen <info@jverdeyen.be>
2013-05-13 10:04:09 +0200

    Added osxfuse download

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,15 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Install osxfuse
+#
+#
+# Examples
+#
+#   include osxfuse
+class osxfuse {
+
+  package { 'OSXFuse':
+    source   => 'http://garr.dl.sourceforge.net/project/osxfuse/osxfuse-2.5.6/osxfuse-2.5.6.dmg',
+    provider => 'pkgdmg'
+  }
+
 }
+
